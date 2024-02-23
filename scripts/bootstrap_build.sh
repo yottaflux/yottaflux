bebc8c58d27fcf8d924807e15f7de434abe57d03
Coop <coop@coopology.com>
2024-02-23 12:44:23 -0600

    Adding scripts for deploy

---
+++ scripts/bootstrap_build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# ubuntu@ip-10-0-1-11:~$ GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=false" git clone git@github.com:yottaflux/yottaflux.git
+
+
+
+sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo apt-get update
+sudo apt-get -y --no-install-recommends install \
+         build-essential \
+         libssl-dev \
+         libboost-all-dev \
+         bison \
+         libexpat1-dev \
+         libdbus-1-dev \
+         libice-dev \
+         libsm-dev \
+         libevent-dev \
+         libminiupnpc-dev \
+         libprotobuf-dev \
+         libqrencode-dev \
+         xcb-proto \
+         zlib1g-dev \
+         libczmq-dev \
+         autoconf \
+         automake \
+         libtool \
+         protobuf-compiler \
+         wget \
+         bsdmainutils \
+         libdb++-dev \
+         qtbase5-dev \
+         qttools5-dev \
+         libfontconfig-dev \
+         libfreetype-dev \
+         libx11-dev \
+         libxau-dev \
+         libxext-dev \
+         libxcb1-dev \
+         libxkbcommon-dev \
+         x11proto-xext-dev \
+         x11proto-dev \
+         xtrans-dev
+
+cd "${HOME}"/yottaflux || exit
+./contrib/install_db4.sh "${HOME}"/
+./autogen.sh
+./configure BDB_LIBS="-L${HOME}/db4/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${HOME}/db4/include" \
+    --disable-test --disable-bench --without-gui
+make -j
+sudo make install
+
+
+
---
+++ scripts/run_yottad.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Pull and build the latest version
+cd /home/ubuntu/yottaflux || exit
+
+sudo -u ubuntu git -C /home/ubuntu/yottaflux pull
+
+systemctl daemon-reload
+
+make
+make install
+
+# check for instance metadata
+
+if [ -z "${ENVIRONMENT}" ]; then
+  ENVIRONMENT=$(curl --silent --fail http://169.254.169.254/latest/meta-data/tags/instance/environment)
+  CURL_RETURN=$?
+
+  if [ $CURL_RETURN -ne 0 ]; then
+    unset ENVIRONMENT
+  fi;
+
+fi;
+
+MY_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+AZ=$(curl  --silent http://169.254.169.254/latest/meta-data/placement/availability-zone)
+REGION=${AZ::-1}
+
+echo Configuring yotta seed for IP $MY_IP , AZ $AZ , in region $REGION - $ENVIRONMENT mode
+
+if [ -z "${ENVIRONMENT}" ]; then
+  mkdir /root/.yottaflux
+  cp scripts/yottaflux.conf /root/.yottaflux/yottaflux.conf
+  exec yottad -printtoconsole
+elif [ "${ENVIRONMENT}" == "dev" ]; then
+  mkdir -p /root/.yottaflux/testnet7
+  cp scripts/yottaflux_test.conf /root/.yottaflux/testnet7/yottaflux.conf
+  exec yottad -testnet -printtoconsole
+elif [ "${ENVIRONMENT}" == "prod" ]; then
+  mkdir /root/.yottaflux
+  cp scripts/yottaflux.conf /root/.yottaflux/yottaflux.conf
+  exec yottad -printtoconsole
+else
+  echo "ERROR: ENVIRONMENT supplied but invalid."
+fi
+
